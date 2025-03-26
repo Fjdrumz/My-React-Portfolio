b953128443a2da0cc05d6fc8713692e3e17e5882
Fjdrumz <fagboyojohnokanmiyo@gmail.com>
2025-03-26 17:16:24 +0100

    Initial commit

---
+++ my-react-portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import NavBar from './components/NavBar';
+import AboutMePage from './components/AboutMePage';
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <NavBar />
+     <AboutMePage />
+    </div>
+  );
+}
+
+export default App
